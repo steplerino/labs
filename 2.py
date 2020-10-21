2aa17a7d3f097e202048f3709243561be515d900
icrazymob <44069831+icrazymob@users.noreply.github.com>
2020-10-21 21:00:34 +0300

    ашч

--- 2.py
+++ 2.py
@@ -1,14 +1,9 @@
 a = {"James", "Ann", "Ruth", "Victor"}
 b = {"Ann", "Victor", "Elizabeth", "Tyson", "Shepard"}
 
-intersect_cardinality = len(a) - len(a - b)
-union_cardinality = len(a) + len(b) - intersect_cardinality
-print("Jaccard", int(intersect_cardinality / union_cardinality * 10000) / 10000)
-
-
 def jaccard_index(first_set, second_set):
+    intersection_cardinality = 0
     for element in first_set:
-        intersection_cardinality = 0
         if element in second_set:
             intersection_cardinality += 1
     return intersection_cardinality / (len(first_set) + len(second_set) - intersection_cardinality)
